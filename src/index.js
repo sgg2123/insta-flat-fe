410cc7efa112c9511f689182ddc40bdc55722d00
sgg2123 <sgg2123@columbia.edu>
2018-06-14 14:52:02 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,76 @@
-//<div class="image-container">
-//    <img src="https://scontent-lga3-1.cdninstagram.com/vp/bd9b15079ec27c52c076e9c7792bdc04/5B992309/t51.2885-15/s640x640/sh0.08/e35/c180.0.719.719/31449135_2115995735352355_6317812590797914112_n.jpg">
-//    <p>
-//        <img data-action="like-image" data-image-id="1" class="like-button" src="./images/like.png"><br>
-//        <span id="likes-count-for-image-1">41</span>
-//    </p>
-//</div>
+const imgAPI = `http://localhost:3000/api/v1/images`
+
+const likesAPI = `http://localhost:3000/api/v1/likes`
+
+const imgContainer = document.getElementById('container')
+
+function getImgs(apiURL){
+  fetch(apiURL).then(r=>r.json()).then(renderImgs)
+}
+
+function renderImgs(imgObjs) {
+  imgObjs.forEach(renderImg)
+}
+
+function renderImg(imgObj) {
+  const div = `<div class="image-container">
+     <img id="${imgObj.id}" src="${imgObj.url}">
+     <p>
+         <img data-action="like-image" data-image-id="${imgObj.id}" class="like-button" src="./images/like.png"><br>
+         <span id="likes-count-for-image-${imgObj.id}">${imgObj.likes_count}</span>
+     </p>
+  </div>`
+  imgContainer.innerHTML += div
+}
+
+getImgs(imgAPI)
+
+imgContainer.addEventListener('click', addLikeOnDOMthenPersist)
+
+function addLikeOnDOMthenPersist(event) {
+  event.preventDefault()
+  const imgId = event.target.dataset.imageId
+  if (event.target.dataset.action){
+    const span = document.getElementById(`likes-count-for-image-${imgId}`)
+    const likeCount = span.innerText
+    span.innerText = parseInt(likeCount) + 1
+  }
+
+  const body = {image_id: imgId}
+  const config = {
+    method: 'POST',
+    headers: {"Content-type":"application/json"},
+    body: JSON.stringify(body)
+  }
+  persistLike(likesAPI, config)
+}
+
+function persistLike(apiURL, config) {
+  fetch(apiURL, config)
+}
+
+function persistPhoto(apiURL, config) {
+  return fetch(apiURL, config).then(r=>r.json())
+}
+
+const newImgForm = document.getElementById('post-image-form')
+
+const newImgURL = document.getElementById('post-image-form-url')
+
+newImgForm.addEventListener('submit', PersistThenAddPhotoOnDOM)
+
+function PersistThenAddPhotoOnDOM(event) {
+  event.preventDefault()
+  const userInput = newImgURL.value
+  const body = {url: userInput}
+  const config = {
+    method: 'POST',
+    headers: {"Content-type":"application/json"},
+    body: JSON.stringify(body)
+  }
+  persistPhoto(imgAPI, config).then(renderImg).then(clearInput)
+}
+
+function clearInput() {
+  newImgURL.value = ''
+}
